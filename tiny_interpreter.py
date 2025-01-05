a8a0d40d62e20411839e8ee2f820db2e8bfba9af
Roberto <robkraudy@gmail.com>
2025-01-05 10:52:35 -0600

    Add the interpreter class

--- tiny_interpreter.py
+++ tiny_interpreter.py
@@ -1,5 +1,5 @@
 """
-This dictionary has 2 arrays that are viewd as 2 stack. The interpreter is considered a stack machine
+This dictionary has 2 arrays that are viewd as 2 stack. This interpreter is considered a stack machine
 """
 what_to_excecute = {
   "instructions": [
@@ -9,6 +9,27 @@ what_to_excecute = {
                   ("ADD_TWO_VALUES", None),
                   ("PRINT_ANSWER", None)
                   ],
-  "number": [7, 5]
+  "numbers": [7, 5]
 }
 
+"""
+Take into account this is not a fast interpreter, which should not be a problem since it is done for learning
+sake
+"""
+
+class Interpreter:
+  def __init__(self):
+    self.stack = [] # Remember this stack is only a list, not the actual memory page segment assigned to the thread
+
+  def LOAD_VALUE(self, number):
+    self.stack.append(number)
+
+  def PRINT_ANSWER(self):
+    answer = self.stack.pop() # We get the last value in the stack
+    print(answer)
+  
+  def ADD_TWO_VALUES(self):
+    first_num = self.stack.pop() # Get the last value from the stack
+    second_num = self.stack.pop() # Get the next value from the stack
+    total = first_num + second_num
+    self.stack.append(total) # Add the output of the operation back to the stack
