ce064c4079a4152bedea970cbff1f9b9e0812644
Roberto <robkraudy@gmail.com>
2025-01-05 11:35:01 -0600

    Add run_code to Interpreter class

--- tiny_interpreter.py
+++ tiny_interpreter.py
@@ -1,7 +1,8 @@
 """
-This dictionary has 2 arrays that are viewd as 2 stack. This interpreter is considered a stack machine
+This dictionary has 2 arrays corresponding to (instruction, argument) and data 
 """
 what_to_excecute = {
+  # This is basically an array of tuples
   "instructions": [
                   # Instruction | data
                   ("LOAD_VALUE", 0), # 0 = First elemnt of the array
@@ -9,18 +10,20 @@ what_to_excecute = {
                   ("ADD_TWO_VALUES", None),
                   ("PRINT_ANSWER", None)
                   ],
+  # This is a simple array
   "numbers": [7, 5]
 }
 
 """
-Take into account this is not a fast interpreter, which should not be a problem since it is done for learning
-sake
+This interpreter is considered a stack machine.Take into account this is not a fast implementation,
+which should not be a problem since it is done for learning sake
 """
 
 class Interpreter:
   def __init__(self):
     self.stack = [] # Remember this stack is only a list, not the actual memory page segment assigned to the thread
 
+  # Add data to the stack
   def LOAD_VALUE(self, number):
     self.stack.append(number)
 
@@ -33,3 +36,19 @@ class Interpreter:
     second_num = self.stack.pop() # Get the next value from the stack
     total = first_num + second_num
     self.stack.append(total) # Add the output of the operation back to the stack
+
+  def run_code(self, what_to_excecute):
+    # Get the instruction values from the dictionary to be interpreted
+    instructions = what_to_excecute["instructions"] 
+    # Get the data stack array 
+    numbers = what_to_excecute["numbers"] 
+    # Now we need to interpret the instructions and apply them on the data
+    for each_step in instructions:
+      instruction, argument = each_step
+      if instruction == "LOAD_VALUE":
+        number = numbers[argument] # Get the data from the array
+        self.LOAD_VALUE(number) # Call the corresponding function
+      elif instruction == "ADD_TWO_VALUES":
+        self.ADD_TWO_VALUES()
+      elif instruction == "PRINT_ANSWER":
+        self.PRINT_ANSWER()
